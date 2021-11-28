e49cb0982f1b7477879b7291a188694eae28e484
KylieReed228 <86483345+KylieReed228@users.noreply.github.com>
2021-11-28 15:32:42 +0300

    sq r3
    sq r3

---
+++ js/app.js
@@ -0,0 +1,33 @@
+import { Swiper, Navigation } from 'swiper'
+Swiper.use([ Navigation, ])
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	// Slider
+
+	const swiper = new Swiper('.reviews-slider', {
+		slidesPerView: 'auto',
+		loop: true,
+		spaceBetween: 30,
+		navigation: {
+			nextEl: '.reviews-slider__button_next',
+			prevEl: '.reviews-slider__button_prev',
+		},
+	})
+
+	// Adaptive menu
+	
+	let navBtn =  document.querySelector('.nav-btn')
+	let nav = document.querySelector('.nav')
+	let body = document.querySelector('body')
+
+	navBtn.addEventListener('click', function(){
+		navBtn.classList.toggle('active')
+		nav.classList.toggle('active')
+		body.classList.toggle('lock')
+	})
+
+})
+
+
